90fa1654f37347c76d23f422e01e57d5e46cf12a
Alina <alineus2001@yandex.ru>
2024-06-08 22:34:53 +0300

    Remove everything

--- WBApp/ContentView.swift
+++ WBApp/ContentView.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        WalkthroughView()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
