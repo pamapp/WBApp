479386e5b5b3039819d8a3e9e4338f6fba8dea73
Alina <alineus2001@yandex.ru>
2024-06-15 11:00:52 +0300

    Add hw2 main view

--- WBApp/ContentView.swift
+++ WBApp/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var router = Router()
+
     var body: some View {
-        WalkthroughView()
+        //        WalkthroughView()
+        
+        TabBarView(tabs: [
+            TabItem(title: "Контакты", imageName: "group", view: AnyView(ContactsView()), route: .contacts),
+            TabItem(title: "Чаты", imageName: "message_circle", view: AnyView(Text("Chats")), route: .chat),
+            TabItem(title: "Ещё", imageName: "more_horizontal", view: AnyView(Text("Settings")), route: .settings)
+        ])
+        .environmentObject(router)
     }
 }
 
 #Preview {
     ContentView()
 }
-
 
 
