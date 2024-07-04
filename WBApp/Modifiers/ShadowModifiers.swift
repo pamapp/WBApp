0798ea8b95466f488da54e5f8f9db9b67b21dcf8
Alina <alineus2001@yandex.ru>
2024-07-04 09:56:31 +0300

    Remove unnecessary

---
+++ WBApp/Modifiers/ShadowModifiers.swift
@@ -0,0 +1,15 @@
+//
+//  ShadowModifiers.swift
+//  WBApp
+//
+//  Created by Alina Potapova on 15.06.2024.
+//
+
+import SwiftUI
+
+struct TabBarShadow: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color.black.opacity(0.04), radius: 20)
+    }
+}
